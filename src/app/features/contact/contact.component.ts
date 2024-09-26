47589c6dbd3227ef5f689edbac6ba7f644d2c472
Welington Junior <welingtonfagundesch@gmail.com>
2024-09-26 02:56:06 -0300

    create the contact component

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild, viewChild } from '@angular/core';
+import { faPhone, faEnvelope, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FontAwesomeModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  @ViewChild('contactForm') contactForm!: NgForm;
 
+  public infoContact = [
+    {
+      icon: faPhone,
+      text: '+55 32 99138-6687'
+    },
+    {
+      icon: faEnvelope,
+      text: 'welingtonfagundesch@gmail.com'
+    },
+    {
+      icon: faMapMarkerAlt,
+      text: 'São Bernardo do Campo, SP, Brazil'
+    }
+  ];
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form submitted:', this.contactForm.value);
+      // Adicione a lógica de envio aqui
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+  
 }
